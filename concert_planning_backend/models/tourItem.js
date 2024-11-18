0692e72e54066b282de3c4f9b5f4b9e7e939b59a
Saurabh-827 <saurabhrabta@gmail.com>
2024-11-18 20:15:44 +0530

    feat: add initial Sequelize setup and create models

---
+++ concert_planning_backend/models/tourItem.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+	const tourItem = sequelize.define(
+		"tourItem",
+		{
+			tourId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: { model: "tour", key: "id" },
+			},
+			itemId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			type: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			timestamps: true,
+		}
+	);
+
+	tourItem.associate = (models) => {
+		tourItem.belongsTo(models.tour, {
+			foreignKey: "tourId",
+		});
+	};
+};

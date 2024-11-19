d25eaf2a9d3d3af3301fecbda672b91c558db5be
Saurabh-827 <saurabhrabta@gmail.com>
2024-11-19 23:55:05 +0530

    feat: add dataController for tour management with create and retrieve functions

---
+++ concert_planning_backend/controllers/dataController.js
@@ -0,0 +1,19 @@
+const {
+	concert: concertModel,
+	afterParties: afterPartiesModel,
+	merchandiseStalls: merchandiseStallsModel,
+	tour: tourModel,
+	tourItem: tourItemModel,
+} = require("../models");
+
+const createTour = async (req, res) => {
+	try {
+	} catch (error) {}
+};
+
+const getTour = async (req, res) => {
+	try {
+	} catch (error) {}
+};
+
+module.exports = { createTour, getTour };

440ea358ee9e4682e7c851c9a9049275a5f3281a
Saurabh-827 <saurabhrabta@gmail.com>
2024-12-10 21:58:14 +0530

    feat: migrate configuration  and set up Sequelize with PostgreSQL

---
+++ 2ndpartlearningagain/backend-users/lib/sequelize.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+const config =
+	require("../config/config")[process.env.NODE_ENV || "development"];
+console.log(`NODE_ENV is set to: ${process.env.NODE_ENV}`);
+
+const sequelize = new Sequelize(
+	config.database,
+	config.username,
+	config.password,
+	{
+		host: config.host,
+		port: config.port,
+		dialect: "postgres",
+		logging: config.logging,
+	}
+);

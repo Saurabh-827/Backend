1f36211ad217c200a23f58cbad4b101b6baf1cd1
Saurabh-827 <saurabhrabta@gmail.com>
2024-12-12 10:35:50 +0530

    feat: implement axios instance for flight search functionality in trip planner

--- 2ndpartlearningagain/trip_planner/index.js
+++ 2ndpartlearningagain/trip_planner/index.js
@@ -1,2 +1,35 @@
+const axiosInstance = require("./lib/axios");
 require("dotenv").config();
-console.log(process.env.client_key);
+
+//checking whether axiosInstance is operational or not
+
+axiosInstance
+	.get("/health")
+	.then((response) => {
+		console.log(response.data);
+	})
+	.catch((error) => {
+		console.log("Error while fetching axiosInstance.", error);
+	});
+
+const getFlights = async (origin, destination) => {
+	try {
+		const response = await axiosInstance.get("/flights/search", {
+			params: {
+				origin: origin,
+				destination: destination,
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+getFlights("bengaluru", "dehradun")
+	.then((flights) => {
+		console.log("Flights are ", flights);
+	})
+	.catch((error) => {
+		console.log("Error while fetching flights.", error);
+	});
---
+++ 2ndpartlearningagain/trip_planner/lib/axios.js
@@ -0,0 +1,14 @@
+const axios = require("axios"); //STEP:1 Install Axios
+require("dotenv").config();
+
+//STEP:2 Create Axios Instance
+
+const axiosInstance = axios.create({
+	baseURL: "https://trip-planner-invact.vercel.app/api/v1",
+	headers: {
+		CLIENT_KEY: process.env.client_key,
+		CLIENT_SECRET: process.env.client_secret,
+	},
+});
+
+module.exports = axiosInstance;

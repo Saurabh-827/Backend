11cc181cb1178d1ef6aca57c6535da3d760260df
Saurabh-827 <saurabhrabta@gmail.com>
2025-02-17 01:32:02 +0530

    feat: add create curated list functionality with validation and error handling

--- 2ndpartlearningagain/MSASSIGNMENTS/cineCuration/index.js
+++ 2ndpartlearningagain/MSASSIGNMENTS/cineCuration/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("./models/index.js");
 const { searchMovie } = require("./controllers/searchMovie.js");
+const { createCuratedList } = require("./controllers/createCuratedList.js");
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/api/movies/search", searchMovie);
+app.post("/api/curated-lists", createCuratedList);
 
 sequelize
 	.authenticate()

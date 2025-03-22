f1efdd06b279e75d4aec380d7423e370c072c543
Saurabh-827 <saurabhrabta@gmail.com>
2025-03-22 23:26:11 +0530

    Added google oauth

--- 2ndpartlearningagain/Oauth/GithubOAuth/oauth-server/index.js
+++ 2ndpartlearningagain/Oauth/GithubOAuth/oauth-server/index.js
@@ -23,6 +23,10 @@ app.get("/auth/github", (req, res) => {
 
 app.get("/auth/github/callback", async (req, res) => {
 	const { code } = req.query;
+	if (!code) {
+		return res.status(400).send("Authorization code is not provided.");
+	}
+	let
 	try {
 		const tokenResponse = await axios.post(
 			"https://github.com/login/oauth/access_token",
@@ -40,6 +44,44 @@ app.get("/auth/github/callback", async (req, res) => {
 		return res.redirect(`${process.env.Fronted_url}/v1/profile/github`);
 	} catch (error) {
 		return res.status(500).json(error);
+	}
+});
+
+
+
+app.get("/auth/google", (req, res) => {
+	const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+	return res.redirect(googleAuthUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+	const { code } = req.query;
+	if (!code) {
+		return res.status(400).send("Authorization code is not provided.");
+	}
+	let accesstoken;
+	try {
+		const tokenResponse = await axios.post(
+			"https://oauth2.googleapis.com/token",
+			{
+				client_id: process.env.GOOGLE_CLIENT_ID,
+				client_secret: process.env.GOOGLE_CLIENT_SECRET,
+				code,
+				grant_type: "authorization_code",
+				redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+			},
+			{
+				headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			}
+		);
+		accesstoken = tokenResponse.data.access_token;
+
+		res.cookie("access_token", accesstoken);
+		return res.redirect(`${process.env.Fronted_url}/v1/profile/google`);
+	} catch (error) {
+		return res
+			.status(500)
+			.json( { error: error.message });
 	}
 });
 
